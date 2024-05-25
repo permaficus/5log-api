fb80245a9e86714b12404ba077459f0dcf0e5097
permaficus <abukhalif2019@gmail.com>
2024-05-26 00:08:16 +0700

    chore(libs): update joi validation schema

--- src/libs/joi.utils.ts
+++ src/libs/joi.utils.ts
@@ -1,1 +1,23 @@
-import Joi from 'joi'
+import Joi, { ValidationError } from 'joi';
+
+const validator = async (schema: any, payload: any) => {
+    return await schema.validateAsync(payload, {
+        abortEarly: true,
+        allowUnknown: false
+    })
+};
+
+const _template_: any = {
+    logLevel: Joi.string().label('Log Level').required(),
+    source: Joi.object().label('Log Source').required(),
+    eventCode: Joi.string().label('Event Code').required(),
+    destination: Joi.string().label('Destination field').allow('').default('N/A'),
+    environment: Joi.string().label('Environment field').allow('').default('N/A').required(),
+    errorDescription: Joi.string().label('Log Description').required()
+}
+const validateSchema = async (payload: any) => {
+    let schema = Joi.object(_template_)
+    return await validator(schema, payload)
+}
+
+export { validateSchema, ValidationError }
