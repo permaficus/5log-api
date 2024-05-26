45e07bc825c41d53849a1dd3f5170f78c99ddf55
permaficus <abukhalif2019@gmail.com>
2024-05-26 09:14:46 +0700

    chore(routes): add routing for GET and DELETE

--- src/v1/routes/detault.ts
+++ src/v1/routes/detault.ts
@@ -1,3 +1,22 @@
 import { Router } from 'express';
+import { PathNotFound, errHandler } from '@/v1/middlewares/errHandler';
+import { challengeAuthentication } from '@/v1/middlewares/authHandler';
+import { collectingLogs, pushLog, removeLogs } from '@/v1/worker/requestHandler';
+import { validateRequest } from '../middlewares/requestValidators';
 
 export const router = Router();
+
+router.post('/logs', challengeAuthentication, validateRequest, pushLog);
+/**
+ * @description: using query params consist of : logID, level, source (eg: hostname, ip, or apps)
+ */
+router.get('/logs', challengeAuthentication, collectingLogs);
+/**
+ * @param: logid / document ID based on mongodb ID
+ */
+router.delete('/logs', challengeAuthentication, removeLogs);
+/**
+ * Error Handling Middleware
+ */
+router.use(PathNotFound);
+router.use(errHandler);
