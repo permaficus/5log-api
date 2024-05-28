1f94937484d51de2d7b522076f2c1e54f5b0077a
permaficus <abukhalif2019@gmail.com>
2024-05-28 12:47:12 +0700

    chore(joi): update joi validation schema and method

--- src/libs/joi.utils.ts
+++ src/libs/joi.utils.ts
@@ -9,14 +9,31 @@ const validator = async (schema: any, payload: any) => {
 
 const _template_: any = {
     logLevel: Joi.string().label('Log Level').required(),
+    logTicket: Joi.string().label('Log Ticket'),
     source: Joi.object().label('Log Source').required(),
     eventCode: Joi.string().label('Event Code').required(),
     destination: Joi.string().label('Destination field').allow('').default('N/A'),
     environment: Joi.string().label('Environment field').allow('').default('N/A').required(),
     errorDescription: Joi.string().label('Log Description').required()
 }
-const validateSchema = async (payload: any) => {
-    let schema = Joi.object(_template_)
+
+const _amqptemplate_: any = {
+    task: Joi.string().valid('POST', 'GET', 'DELETE').label('Task').required(),
+    payload: Joi.object(_template_).required(),
+    origin: Joi.object({
+        queue: Joi.string().label('Queue Name').required(),
+        routingKey: Joi.string().label('Message routing key').required(),
+        message: Joi.object().label('Respond message').optional(),
+        fallback: Joi.object({
+            url: Joi.string().label('Fallback URL').required(),
+            method: Joi.string().valid('POST').required(),
+            headers: Joi.string().optional()
+        }).optional()
+    }).label('Origin').required().unknown(false)
+}
+
+const validateSchema = async (payload: any, protocol?: 'amqp' | 'http') => {
+    let schema = protocol !== 'amqp' ? Joi.object(_template_) : Joi.object(_amqptemplate_).fork('payload.logTicket', (field) => field.required())
     return await validator(schema, payload)
 }
 
