a9e15928401de0056a4ec0393c0d7be5a0f14f3b
permaficus <abukhalif2019@gmail.com>
2024-06-16 09:48:46 +0700

    chore(middleware): update on authHandler

--- src/v1/middlewares/authHandler.ts
+++ src/v1/middlewares/authHandler.ts
@@ -3,7 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export const challengeAuthentication = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     if (!req.headers.client_id) {
         res.status(401);
-        next(new Error(`Authentication Failed - ${req.originalUrl}`))
+        next(new Error(`Authentication Failed - ${req.originalUrl}`));
+        return;
     }
     // next contact auth service
     next();
