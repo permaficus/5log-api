b8f0e670358a016b75facc1ccff0aa78e64b0105
permaficus <abukhalif2019@gmail.com>
2024-06-19 10:33:13 +0700

    chore(middleware): update non-relative path import

--- src/v1/routes/detault.ts
+++ src/v1/routes/detault.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { PathNotFound, errHandler } from '@/v1/middlewares/errHandler';
 import { challengeAuthentication } from '@/v1/middlewares/authHandler';
 import { handleHttpRequest} from '@/v1/worker/requestHandler';
-import { validateRequest } from '../middlewares/requestValidators';
-import { readFromCache } from '../middlewares/cache';
+import { validateRequest } from '@/v1/middlewares/requestValidators';
+import { readFromCache } from '@/v1/middlewares/cache';
 
 export const router = Router();
 
